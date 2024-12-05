cd473e66315792f6b5d3eb26701b07c3f7cc87e8
William Wolke <60339769+William-Wolke@users.noreply.github.com>
2024-12-05 20:06:40 +0100

    2024 day 3 part 1 c#

---
+++ 2024/3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main()
+        {
+            Part1();
+        }
+        static void Part1()
+        {
+            String? line;
+            MatchCollection matches;
+            MatchCollection numbers;
+            Int32 left_num;
+            Int32 right_num;
+            Int32 tot_sum = 0;
+            Regex regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+            Regex regex2 = new Regex(@"\d{1,3}");
+            StreamReader sr = new StreamReader("./input.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                matches = regex.Matches(line);
+                foreach (Match match in matches)
+                {
+                    numbers = regex2.Matches(match.Value);
+                    left_num = Int32.Parse(numbers[0].Value);
+                    right_num = Int32.Parse(numbers[1].Value);
+                    tot_sum += left_num * right_num;
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.WriteLine(tot_sum);
+            Console.ReadLine();
+        }
+    }
+}
+
