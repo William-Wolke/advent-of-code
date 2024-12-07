b757b7b220d37af8fcfec122b3a6821ca5aa582e
William Wolke <60339769+William-Wolke@users.noreply.github.com>
2024-12-07 17:58:47 +0100

    day 3 part 2

--- 2024/3/Program.cs
+++ 2024/3/Program.cs
@@ -8,6 +8,7 @@ namespace Name
         static void Main()
         {
             Part1();
+            Part2();
         }
         static void Part1()
         {
@@ -35,7 +36,37 @@ namespace Name
             }
             sr.Close();
             Console.WriteLine(tot_sum);
-            Console.ReadLine();
+        }
+        static void Part2() {
+            String lines = "";
+            String? line;
+            MatchCollection numbers;
+            Int32 left_num;
+            Int32 right_num;
+            Int32 tot_sum = 0;
+            Regex regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+            Regex regex2 = new Regex(@"\d{1,3}");
+            Regex regex3 = new Regex(@"don't\(\)[\s\S]*?do\(\)");
+            StreamReader sr = new StreamReader("./input.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                lines += line;
+                line = sr.ReadLine();
+            }
+            foreach (Match match in regex3.Matches(lines))
+            {
+                lines = lines.Replace(match.Value, "");
+            }
+            foreach (Match match in regex.Matches(lines))
+            {
+                numbers = regex2.Matches(match.Value);
+                left_num = Int32.Parse(numbers[0].Value);
+                right_num = Int32.Parse(numbers[1].Value);
+                tot_sum += left_num * right_num;
+            }
+            sr.Close();
+            Console.WriteLine(tot_sum);
         }
     }
 }
