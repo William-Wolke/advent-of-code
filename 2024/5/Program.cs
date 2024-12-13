c5db752d14f7ac8ba89cf0e4c36c932957736345
William Wolke <60339769+William-Wolke@users.noreply.github.com>
2024-12-13 08:10:49 +0100

    day 5 part 2

--- 2024/5/Program.cs
+++ 2024/5/Program.cs
@@ -11,6 +11,7 @@ class Program
         List<Int32[]> rulePairs = new List<Int32[]>();
         List<Int32[]> pageUpdates = new List<Int32[]>();
         List<Int32[]> invalidPageUpdates = new List<Int32[]>();
+        List<Int32[]> correctedPageUpdates = new List<Int32[]>();
         List<Int32> validMiddlePageUpdates = new List<Int32>();
         String[] lines = File.ReadAllLines("./input.txt");
         foreach (string line in lines)
@@ -54,8 +55,25 @@ class Program
                 Int32 middle = Decimal.ToInt32(Math.Ceiling(count));
                 validMiddlePageUpdates.Add(pages[middle]);
             }
+            else
+            {
+                invalidPageUpdates.Add(pages);
+            }
+        }
+        foreach (Int32[] invalidPages in invalidPageUpdates)
+        {
+            correctedPageUpdates.Add(SortPageUpdates(invalidPages, rulePairs));
         }
         Int32 sum = validMiddlePageUpdates.Sum();
+        Console.WriteLine(sum);
+        validMiddlePageUpdates = new List<Int32>();
+        foreach (Int32[] validPages in correctedPageUpdates)
+        {
+            Decimal count = validPages.Count()/2;
+            Int32 middle = Decimal.ToInt32(Math.Ceiling(count));
+            validMiddlePageUpdates.Add(validPages[middle]);
+        }
+        sum = validMiddlePageUpdates.Sum();
         Console.WriteLine(sum);
     }
     static Int32? IndexOf(Int32[] arr, Int32 value)
@@ -70,5 +88,35 @@ class Program
             i++;
         }
         return null;
+    }
+    static Int32[] SortPageUpdates(Int32[] invalidPages, List<Int32[]> rulePairs)
+    {
+        Boolean isSorted = false;
+        while (!isSorted)
+        {
+            Boolean isChanged = false;
+            foreach (Int32[] rulePair in rulePairs)
+            {
+                var indexRule0 = IndexOf(invalidPages, rulePair[0]);
+                var indexRule1 = IndexOf(invalidPages, rulePair[1]);
+                if (indexRule0 is null || indexRule1 is null)
+                {
+                    continue;
+                }
+                if (indexRule0 < indexRule1)
+                {
+                    continue;
+                }
+                var temp = invalidPages[indexRule0.Value];
+                invalidPages[indexRule0.Value] = invalidPages[indexRule1.Value];
+                invalidPages[indexRule1.Value] = temp;
+                isChanged = true;
+            }
+            if (!isChanged)
+            {
+                isSorted = true;
+            }
+        }
+        return invalidPages;
     }
 }
